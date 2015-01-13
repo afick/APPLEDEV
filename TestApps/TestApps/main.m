4a12c33cf3016dd5b99edb059c1bf9fd05c49565
afick <akfick@gmail.com>
2015-01-13 16:39:00 -0600

    Testing for OBJECTIVE C

---
+++ TestApps/TestApps/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestApps
+//
+//  Created by Mandy Fick on 1/13/15.
+//  Copyright (c) 2015 Mandy Fick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
